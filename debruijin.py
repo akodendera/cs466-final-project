196d3996ac736cf3b97e542b9001233b85a66fd6
Minhyuk Park <minpark815@gmail.com>
2019-04-28 04:54:49 +0000

    Import to pandas

--- debruijin.py
+++ debruijin.py
@@ -1,7 +1,23 @@
+import pprint
 from Bio import SeqIO
+import pandas as pd
+from tabulate import tabulate
+pp = pprint.PrettyPrinter(indent=4)
 
 filename = 'Chelonoidis_abingdonii.ASM359739v1.pep.abinitio.fa'
-sequence = SeqIO.parse(open(filename), 'fasta')
 
+df = pd.DataFrame()
+
+sequence = SeqIO.parse(open(filename), 'fasta')
+counter = 0
 for record in sequence:
-    print(record.id)
+    df = df.append({
+        'sequence_id': record.id,
+        'sequence': str(record.seq)
+    }, ignore_index=True)
+    counter += 1
+    if(counter % 10000 == 0):
+        print(counter)
+
+print(df.head)
+print(tabulate(df, headers='keys', tablefmt='psql'))
