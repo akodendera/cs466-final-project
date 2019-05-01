9d20c4ef1870667b52b024ae55051f1e982827d6
Minhyuk Park <minpark815@gmail.com>
2019-05-01 00:50:21 +0000

    Include naive eulerian algorithm

--- debruijingraph.py
+++ debruijingraph.py
@@ -1,10 +1,13 @@
 # from graphviz import Digraph
+from random import shuffle
 
 
 class DebruijinGraph:
     def __init__(self, sequence, k):
         self.nodes = set()
         self.edges = set()  # directed set of tuples (source, destination)
+        self.read_size = k
+        self.sequence = sequence
         self.initializeDebruijinGraph(sequence, k)
         self.naive_eulerian_path = None
         self.smart_eulerian_path = None
@@ -46,45 +49,91 @@ class DebruijinGraph:
         print("Warning: {}".format(s))
 
     def get_overlap(self, left, right):
-        score = 0
-        for l, r in zip(reversed(left), right):
-            if(l == r):
-                score += 1
-        return score
+        max_score = 0
+        # print("checking overlap of " + repr(left) + " and " + repr(right))
+        # 10 and 11
+        for cur_score in xrange(1, min(len(left), len(right)) + 1):
+            cur_alignment_score = 0
+            for cur_align_index in xrange(cur_score):
+                if(left[len(left) - cur_score + cur_align_index] == right[cur_align_index]):
+                    # print("increase overlap")
+                    cur_alignment_score += 1
+                else:
+                    # print("no more overlap")
+                    break
+            if cur_alignment_score != cur_score:
+                cur_alignment_score = 0
+
+            max_score = max(max_score, cur_alignment_score)
+            # print("current max score is " + repr(max_score))
+
+        # for cur_alignment_index in xrange(self.read_size - 2):
+        #     if(left[len(left) - (self.read_size - 2) + cur_alignment_index] == right[cur_alignment_index]):
+        #         # print("increase overlap")
+        #         max_score += 1
+        #     else:
+        #         # print("no more overlap")
+        #         break
+
+        # if(max_score < self.read_size - 2):
+        #     max_score = 0
+        return max_score
 
     def find_naive_eulerian_path(self):
         """ find the eulerian path using a naive algorithm """
-        path = ""
-        merged_nodes = set()
+        merged_nodes = []
         for i in self.nodes:
-            merged_nodes.add(i)
+            merged_nodes.append(i)
+        for i in self.edges:
+            merged_nodes.append(i[1])
 
-        while(len(merged_nodes) != 0):
-            current_node = merged_node.pop()
+        while(len(merged_nodes) != 1):
+            shuffle(merged_nodes)
+            current_node = merged_nodes.pop()
+            # print("starting merge")
+            # print(repr(current_node))
             max_overlap_score = -1
             max_overlap_node = None
             for i in merged_nodes:
-                left_overlap = self.get_overlap(i, current_node)
-                right_overlap = self.get_overlap(current_node, i)
-                left_overlap_score = left_overlap[0]
-                right_overlap_score = right_overlap[0]
-                if(left_overlap_score < right_overlap_score):
-                    max_overlap_score = right_overlap_score
-                    max_overlap_node = (i, current_node)
-                else:
-                    max_overlap_score = left_overlap_score
-                    max_overlap_node = (current_node, i)
+                left_overlap_score = self.get_overlap(i, current_node)
+                right_overlap_score = self.get_overlap(current_node, i)
+                if(max_overlap_score < max(left_overlap_score, right_overlap_score)):
+                    if(left_overlap_score < right_overlap_score):
+                        max_overlap_score = right_overlap_score
+                        max_overlap_node = (current_node, i)
+                    else:
+                        max_overlap_score = left_overlap_score
+                        max_overlap_node = (i, current_node)
+            # if max_overlap_score < self.read_size - 2:
+            if max_overlap_score == -1:
+                # print("could not merge any more")
+                return
 
-            merged_nodes.remove(max_overlap_node[0])
-            merged_nodes.remove(max_overlap_node[1])
-            merged_nodes.add(max_overlap_node[0][:len(max_overlap_node[0]) - max_overlap_score]
-                             + max_overlap_node[1][max_overlap_score:])
+            # print("merging " + repr(max_overlap_node) + " with a score of " + repr(max_overlap_score))
+            if(max_overlap_node[0] == current_node):
+                merged_nodes.remove(max_overlap_node[1])
+            else:
+                merged_nodes.remove(max_overlap_node[0])
 
-        self.naive_eulerian_path = path
+            merged_nodes.append(max_overlap_node[0] + max_overlap_node[1][max_overlap_score:])
+            # print("new node " + repr(max_overlap_node[0] + max_overlap_node[1][max_overlap_score:]))
+
+        result = merged_nodes.pop()
+        if(self.is_eulerian(result)):
+            self.naive_eulerian_path = result
+        else:
+            self.naive_eulerian_path = "The naive greedy algorithm failed to find a eulerian path"
+
+    def is_eulerian(self, path):
+        kmer_list = self.getkmerList(path, self.read_size)
+        for i in kmer_list:
+            lmer, rmer = self.getLeftRightMers(i)
+            if((lmer,rmer) not in self.edges):
+                return False
+        return len(path) == len(self.sequence)
 
     def find_smart_eulerian_path(self):
         """ find the eulerian path using a smart algorithm """
         path = ""
         self.smart_eulerian_path = path
 
-# if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from sequences import Sequences
 from debruijingraph import DebruijinGraph
 
-N_THREADS = 20
-FILENAME = 'Chelonoidis_abingdonii.ASM359739v1.pep.abinitio.fa'
-debroin = Sequences(N_THREADS, FILENAME)
-print(debroin.df.shape[0])
+# N_THREADS = 20
+# FILENAME = 'Chelonoidis_abingdonii.ASM359739v1.pep.abinitio.fa'
+# debroin = Sequences(N_THREADS, FILENAME)
+# print(debroin.df.shape[0])
 
 # s2 = debroin.df['sequence'].iloc[0]
 # g2 = makeDebruijinGraph(s2, 3)
 # g2.printGraph()
-three_mer_graph_list = []
+# three_mer_graph_list = []
 # for i in range(debroin.df.shape[0]):
-for i in range(200):
-    s = debroin.df['sequence'].iloc[i]
-    three_mer_graph_list.append(DebruijinGraph(s, 3))
+# for i in range(200):
+#     s = debroin.df['sequence'].iloc[i]
+#     three_mer_graph_list.append(DebruijinGraph(s, 3))
 
-print len(three_mer_graph_list)
+s3 = "abccde"
+g3 = DebruijinGraph(s3, 4)
+g3.printGraph()
+
+g3.find_naive_eulerian_path()
+print(repr(g3.naive_eulerian_path))
+
+s4 = "0011101000"
+g4 = DebruijinGraph(s4, 3)
+g4.printGraph()
+
+g4.find_naive_eulerian_path()
+print(repr(g4.naive_eulerian_path))
